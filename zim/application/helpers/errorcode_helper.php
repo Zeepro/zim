efb038b2577f3f5665e75548d5325979821a2c8b
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2013-12-27 09:14:13 +0000

    Add an error list to manage error message and code

---
+++ zim/application/helpers/errorcode_helper.php
@@ -0,0 +1,90 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+
+if (!defined(ERROR_OK)) {
+	define("ERROR_OK",				200);
+	define("ERROR_MISS_PRM",		432);
+	define("ERROR_WRONG_PRM",		433);
+	define("ERROR_LOADED_UNLOAD",	434);
+	define("ERROR_IN_PRINT",		435);
+	define("ERROR_WRONG_PWD",		436);
+	define("ERROR_NO_PRINT",		437);
+	define("ERROR_MISS_CARTRIDGE",	438);
+	define("ERROR_MISS_FILAMENT",	439);
+	define("ERROR_PRES_FILAMENT",	440);
+// 	define("ERROR_WRONG_FORMAT",	440);
+	define("ERROR_EMPTY_PLATFORM",	441);
+	define("ERROR_MISS_LEFT_FILA",	442);
+	define("ERROR_MISS_RIGT_FILA",	443);
+	define("ERROR_LOW_LEFT_FILA",	444);
+	define("ERROR_LOW_RIGT_FILA",	445);
+	define("ERROR_BUSY_PRINTER",	446);
+	define("ERROR_FULL_PRTLST",		447);
+	define("ERROR_UNKNOWN_MODEL",	448);
+	define("ERROR_UNKNOWN_PIC",		449);
+	define("ERROR_TOOBIG_FILE",		450);
+	define("ERROR_TOOBIG_MODEL",	451);
+	define("ERROR_IN_UPDATE",		452);
+	
+	$MY_ERRMSG_ARRAY = array (
+		ERROR_OK				=> "Ok",
+		ERROR_MISS_PRM			=> "Missing parameter",
+		ERROR_WRONG_PRM			=> "Incorrect parameter",
+		ERROR_LOADED_UNLOAD		=> "Already loaded / unloaded",
+		ERROR_IN_PRINT			=> "Printing in progress",
+		ERROR_WRONG_PWD			=> "Missing / Incorrect password",
+		ERROR_NO_PRINT			=> "No current printing",
+		ERROR_MISS_CARTRIDGE	=> "Cartridge missing",
+		ERROR_MISS_FILAMENT		=> "Filament missing",
+		ERROR_PRES_FILAMENT		=> "Filament present",
+// 		ERROR_WRONG_FORMAT		=> "Incorrect format",
+		ERROR_EMPTY_PLATFORM	=> "Platform empty",
+		ERROR_MISS_LEFT_FILA	=> "Left filament missing",
+		ERROR_MISS_RIGT_FILA	=> "Right filament missing",
+		ERROR_LOW_LEFT_FILA		=> "Not enough left filament",
+		ERROR_LOW_RIGT_FILA		=> "Not enough right filament",
+		ERROR_BUSY_PRINTER		=> "Printer busy",
+		ERROR_FULL_PRTLST		=> "Print list full",
+		ERROR_UNKNOWN_MODEL		=> "Unknown model",
+		ERROR_UNKNOWN_PIC		=> "Unknown picture",
+		ERROR_TOOBIG_FILE		=> "File too big", //"to" -> "too", error in spec
+		ERROR_TOOBIG_MODEL		=> "Model too big", // same above
+		ERROR_IN_UPDATE			=> "Update in progress",
+	);
+	
+	function MyERRMSG($CODEERR) {
+		return t($MY_ERRMSG_ARRAY[$CODEERR]);
+	}
+	
+	// example:
+	// printf("%d: %s", ERROR_OK, MyERRMSG(ERROR_OK));
+	// echo (ERROR_OK . ": " . MyERRMSG(ERROR_OK));
+}
+
+/* another way to build an error list
+ * but too long to call this class
+if (!class_exists(MY_ErrorCode)) {
+	abstract class MY_ErrorCode {
+		const ERROR_MISS_PRM		= 432;
+		const ERROR_WRONG_PRM		= 433;
+		const ERROR_LOADED_UNLOAD	= 434;
+		const ERROR_IN_PRINT		= 435;
+		const ERROR_WRONG_PWD		= 436;
+		const ERROR_NO_PRINT		= 437;
+		const ERROR_MISS_CARTRIDGE	= 438;
+		const ERROR_MISS_FILAMENT	= 439;
+		const ERROR_PRES_FILAMENT	= 440;
+		const ERROR_WRONG_FORMAT	= 440;
+		
+		const ERRMSG_ARRAY = array (
+			ERROR_MISS_PRM		=> "Missing parameter",
+		);
+	}
+	// example:
+	// MY_ErrorCode::ERROR_IN_PRINT;
+	// MY_ErrorCode::ERRMSG_ARRAY[MY_ErrorCode::ERROR_IN_PRINT];
+}
+ */
