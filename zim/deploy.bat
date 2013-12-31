af3322c6822ef4e52d6ee2c4f4dc969e0a44c4c9
zoombai <zoombai@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2013-12-31 15:17:57 +0000

    Config should not be copied

--- zim/deploy.bat
+++ zim/deploy.bat
@@ -1,2 +1,2 @@
-robocopy C:\inetpub\wwwroot\zim\uidev C:\inetpub\wwwroot\zim\ui *.* /e /purge /xf .* *.bat *.bak /xd .* /m
+robocopy c:\inetpub\wwwroot\zim\uidev c:\inetpub\wwwroot\zim\ui *.* /e /purge /xf .* *.bat *.bak C:\inetpub\wwwroot\zim\uidev\application\config\config.php /xd .* data /m
 pause
