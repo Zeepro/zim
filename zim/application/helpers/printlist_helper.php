bef9fc5d24468ec71a211e62c2e95a784f9f5fc4
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-01-09 09:37:17 +0000

    improve print list helper more logic, and we can get data in array now make the blind url security issue as an internal function from internal command add an internal function to change english color name to hex rgb code

--- zim/application/helpers/printlist_helper.php
+++ zim/application/helpers/printlist_helper.php
@@ -125,11 +125,17 @@ function ModelList_add($data_array) {
 			//model filament1 length
 			if (isset($data_array['c1'])) {
 				$model_color1 = $data_array['c1'];
+				if (ModelList__changeColorName($model_color1) != ERROR_OK) {
+					return ERROR_WRONG_PRM;
+				}
 			}
 
 			//model filament1 length
 			if (isset($data_array['c2'])) {
 				$model_color2 = $data_array['c2'];
+				if (ModelList__changeColorName($model_color2) != ERROR_OK) {
+					return ERROR_WRONG_PRM;
+				}
 			}
 			
 			//model picture 1
@@ -194,7 +200,7 @@ function ModelList_add($data_array) {
 	//==========================================================
 	//model name, description, duration, filament1+2
 	$json_data = array(
-			PRINTLIST_TITLE_ID			=> md5($model_name),
+			PRINTLIST_TITLE_ID			=> md5(utf8_decode($model_name)),
 			PRINTLIST_TITLE_NAME		=> $model_name,
 			PRINTLIST_TITLE_DESP		=> $model_desp,
 			PRINTLIST_TITLE_TIME		=> $model_printtime,
@@ -255,42 +261,9 @@ function ModelList_delete($id_model) {
 }
 
 function ModelList_list() {
-	global $CFG;
-	$printlist_basepath	= $CFG->config['printlist'];
-	$json_data = array();
-	$tmp_array = NULL;
+	$array_data = ModelList__listAsArray();
 	
-	$model_array = directory_map($printlist_basepath, 1);
-	foreach ($model_array as $model_name) {
-		$model_path = $printlist_basepath . $model_name . '/';
-		$nb_pic = 0;
-		
-		try {
-			$tmp_array = json_read($model_path . PRINTLIST_FILE_JSON, TRUE);
-			if ($tmp_array['error']) {
-				throw new Exception('read json error');
-			}
-		} catch (Exception $e) {
-			continue; // just jump through the wrong data file
-// 			return json_encode($json_data); //TODO how about return ERROR_INTERNAL here?
-		}
-		$tmp_array['json'][PRINTLIST_TITLE_ID] = md5($model_name); //add model id to data array
-		
-		//blind picture url
-		if (isset($tmp_array['json'][PRINTLIST_TITLE_PIC])
-				&& count($tmp_array['json'][PRINTLIST_TITLE_PIC])) {
-			$nb_pic = count($tmp_array['json'][PRINTLIST_TITLE_PIC]);
-			for ($i=0; $i < $nb_pic; $i++) { //we cannot use foreach to change value
-				$tmp_array['json'][PRINTLIST_TITLE_PIC][$i] = PRINTLIST_GETPIC_BASE_WEB
-					. '?' . PRINTLIST_GETPIC_PRM_MID . '=' . md5($model_name)
-					. '&' . PRINTLIST_GETPIC_PRM_PIC . '=' . ($i + 1);
-			}
-		}
-		
-		$json_data[] = $tmp_array['json']; //asign final data
-	}
-	
-	return json_unicode_decode(json_encode($json_data));
+	return json_unicode_decode(json_encode($array_data));
 }
 
 function ModelList_getPic($id_model, $id_picture, &$path_pid) {
@@ -407,10 +380,67 @@ function ModelList__find($id_model_find, &$model_path) {
 	return ERROR_OK;
 }
 
-function ModelList__getDuration($id_model, &$duration) {
-	//TODO finish this function if necessary
-	// but now, print presliced file always finish in 20s (random select between 0 and 20)
-	$json_data = NULL;
+function ModelList__listAsArray() {
+	global $CFG;
+	$printlist_basepath	= $CFG->config['printlist'];
+	$json_data = array();
+	$tmp_array = NULL;
+	
+	$model_array = directory_map($printlist_basepath, 1);
+	foreach ($model_array as $model_name) {
+		$model_path = $printlist_basepath . $model_name . '/';
+		$nb_pic = 0;
+		
+		try {
+			$tmp_array = json_read($model_path . PRINTLIST_FILE_JSON, TRUE);
+			if ($tmp_array['error']) {
+				throw new Exception('read json error');
+			}
+		} catch (Exception $e) {
+			continue; // just jump through the wrong data file
+// 			return json_encode($json_data); //TODO how about return ERROR_INTERNAL here?
+		}
+// 		$tmp_array['json'][PRINTLIST_TITLE_ID] = md5($model_name); //add model id to data array
+		
+		//blind picture url
+		ModelList__blindUrl($tmp_array['json']);
+// 		if (isset($tmp_array['json'][PRINTLIST_TITLE_PIC])
+// 				&& count($tmp_array['json'][PRINTLIST_TITLE_PIC])) {
+// 			$nb_pic = count($tmp_array['json'][PRINTLIST_TITLE_PIC]);
+// 			for ($i=0; $i < $nb_pic; $i++) { //we cannot use foreach to change value
+// 				$tmp_array['json'][PRINTLIST_TITLE_PIC][$i] = PRINTLIST_GETPIC_BASE_WEB
+// 					. '?' . PRINTLIST_GETPIC_PRM_MID . '=' . md5($model_name)
+// 					. '&' . PRINTLIST_GETPIC_PRM_PIC . '=' . ($i + 1);
+// 			}
+// 		}
+		
+		$json_data[] = $tmp_array['json']; //asign final data
+	}
+	
+	return $json_data;
+}
+
+function ModelList__blindUrl(&$array_json) {
+	$model_id = $array_json[PRINTLIST_TITLE_ID];
+	
+	if (isset($array_json[PRINTLIST_TITLE_PIC])
+			&& count($array_json[PRINTLIST_TITLE_PIC])) {
+		$nb_pic = count($array_json[PRINTLIST_TITLE_PIC]);
+		for ($i=0; $i < $nb_pic; $i++) { //we cannot use foreach to change value
+			$array_json[PRINTLIST_TITLE_PIC][$i] = PRINTLIST_GETPIC_BASE_WEB
+				. '?' . PRINTLIST_GETPIC_PRM_MID . '=' . $model_id
+				. '&' . PRINTLIST_GETPIC_PRM_PIC . '=' . ($i + 1);
+		}
+	}
+	
+	return;
+}
+
+function ModelList__getDetailAsArray($id_model, &$array_data) {
+	//TODO add it to spec file if necessary
+	// but for web service, it's better to return json,
+	// so we need ModelList_getDetail() for web service
+	$array_data = NULL;
 	$tmp_array = NULL;
 	$json_path = NULL;
 	$model_path = NULL;
@@ -418,17 +448,191 @@ function ModelList__getDuration($id_model, &$duration) {
 	$model_cr = ModelList__find($id_model, $model_path);
 	if (($model_cr == ERROR_OK) && $model_path) {
 		$json_path = $model_path . PRINTLIST_FILE_JSON;
-		$tmp_array = json_read($json_path);
+		$tmp_array = json_read($json_path, TRUE);
 		if ($tmp_array['error']) {
 			return ERROR_INTERNAL;
 		}
-		$json_data = $tmp_array['json'];
-		$duration = $json_data[PRINTLIST_TITLE_TIME];
-	
+		$array_data = $tmp_array['json'];
+		ModelList__blindUrl($array_data);
+		
 		return ERROR_OK;
 	} else {
 		return ERROR_UNKNOWN_MODEL;
 	}
 	
 	return;
+}
+
+function ModelList__getDuration($id_model, &$duration) {
+	//TODO finish this function if necessary
+	// but now, print presliced file always finish in 20s (random select between 0 and 20)
+	$json_data = NULL;
+
+	$model_cr = ModelList__getDetailAsArray($id_model, $json_data);
+	if (($model_cr == ERROR_OK) && $json_data) {
+		$duration = $json_data[PRINTLIST_TITLE_TIME];
+	
+		return ERROR_OK;
+	} else {
+		return $model_cr;
+	}
+	
+	return;
+}
+
+function ModelList__changeColorName(&$color_code) {
+	if ($color_code[0] == '#') {
+		return ERROR_OK;
+	} // return directly if color is already in hex code
+	
+	$array_color = array (
+			'aliceblue'				=> '#f0f8ff',
+			'antiquewhite'			=> '#faebd7',
+			'aqua'					=> '#00ffff',
+			'aquamarine'			=> '#7fffd4',
+			'azure'					=> '#f0ffff',
+			'beige'					=> '#f5f5dc',
+			'bisque'				=> '#ffe4c4',
+			'black'					=> '#000000',
+			'blanchedalmond'		=> '#ffebcd',
+			'blue'					=> '#0000ff',
+			'blueviolet'			=> '#8a2be2',
+			'brown'					=> '#a52a2a',
+			'burlywood'				=> '#deb887',
+			'cadetblue'				=> '#5f9ea0',
+			'chartreuse'			=> '#7fff00',
+			'chocolate'				=> '#d2691e',
+			'coral'					=> '#ff7f50',
+			'cornflowerblue'		=> '#6495ed',
+			'cornsilk'				=> '#fff8dc',
+			'crimson'				=> '#dc143c',
+			'cyan'					=> '#00ffff',
+			'darkblue'				=> '#00008b',
+			'darkcyan'				=> '#008b8b',
+			'darkgoldenrod'			=> '#b8860b',
+			'darkgray'				=> '#a9a9a9',
+			'darkgreen'				=> '#006400',
+			'darkkhaki'				=> '#bdb76b',
+			'darkmagenta'			=> '#8b008b',
+			'darkolivegreen'		=> '#556b2f',
+			'darkorange'			=> '#ff8c00',
+			'darkorchid'			=> '#9932cc',
+			'darkred'				=> '#8b0000',
+			'darksalmon'			=> '#e9967a',
+			'darkseagreen'			=> '#8fbc8f',
+			'darkslateblue'			=> '#483d8b',
+			'darkslategray'			=> '#2f4f4f',
+			'darkturquoise'			=> '#00ced1',
+			'darkviolet'			=> '#9400d3',
+			'deeppink'				=> '#ff1493',
+			'deepskyblue'			=> '#00bfff',
+			'dimgray'				=> '#696969',
+			'dodgerblue'			=> '#1e90ff',
+			'firebrick'				=> '#b22222',
+			'floralwhite'			=> '#fffaf0',
+			'forestgreen'			=> '#228b22',
+			'fuchsia'				=> '#ff00ff',
+			'gainsboro'				=> '#dcdcdc',
+			'ghostwhite'			=> '#f8f8ff',
+			'gold'					=> '#ffd700',
+			'goldenrod'				=> '#daa520',
+			'gray'					=> '#808080',
+			'green'					=> '#008000',
+			'greenyellow'			=> '#adff2f',
+			'honeydew'				=> '#f0fff0',
+			'hotpink'				=> '#ff69b4',
+			'indianred'				=> '#cd5c5c',
+			'indigo '				=> '#4b0082',
+			'ivory'					=> '#fffff0',
+			'khaki'					=> '#f0e68c',
+			'lavender'				=> '#e6e6fa',
+			'lavenderblush'			=> '#fff0f5',
+			'lawngreen'				=> '#7cfc00',
+			'lemonchiffon'			=> '#fffacd',
+			'lightblue'				=> '#add8e6',
+			'lightcoral'			=> '#f08080',
+			'lightcyan'				=> '#e0ffff',
+			'lightgoldenrodyellow'	=> '#fafad2',
+			'lightgrey'				=> '#d3d3d3',
+			'lightgreen'			=> '#90ee90',
+			'lightpink'				=> '#ffb6c1',
+			'lightsalmon'			=> '#ffa07a',
+			'lightseagreen'			=> '#20b2aa',
+			'lightskyblue'			=> '#87cefa',
+			'lightslategray'		=> '#778899',
+			'lightsteelblue'		=> '#b0c4de',
+			'lightyellow'			=> '#ffffe0',
+			'lime'					=> '#00ff00',
+			'limegreen'				=> '#32cd32',
+			'linen'					=> '#faf0e6',
+			'magenta'				=> '#ff00ff',
+			'maroon'				=> '#800000',
+			'mediumaquamarine'		=> '#66cdaa',
+			'mediumblue'			=> '#0000cd',
+			'mediumorchid'			=> '#ba55d3',
+			'mediumpurple'			=> '#9370d8',
+			'mediumseagreen'		=> '#3cb371',
+			'mediumslateblue'		=> '#7b68ee',
+			'mediumspringgreen'		=> '#00fa9a',
+			'mediumturquoise'		=> '#48d1cc',
+			'mediumvioletred'		=> '#c71585',
+			'midnightblue'			=> '#191970',
+			'mintcream'				=> '#f5fffa',
+			'mistyrose'				=> '#ffe4e1',
+			'moccasin'				=> '#ffe4b5',
+			'navajowhite'			=> '#ffdead',
+			'navy'					=> '#000080',
+			'oldlace'				=> '#fdf5e6',
+			'olive'					=> '#808000',
+			'olivedrab'				=> '#6b8e23',
+			'orange'				=> '#ffa500',
+			'orangered'				=> '#ff4500',
+			'orchid'				=> '#da70d6',
+			'palegoldenrod'			=> '#eee8aa',
+			'palegreen'				=> '#98fb98',
+			'paleturquoise'			=> '#afeeee',
+			'palevioletred'			=> '#d87093',
+			'papayawhip'			=> '#ffefd5',
+			'peachpuff'				=> '#ffdab9',
+			'peru'					=> '#cd853f',
+			'pink'					=> '#ffc0cb',
+			'plum'					=> '#dda0dd',
+			'powderblue'			=> '#b0e0e6',
+			'purple'				=> '#800080',
+			'red'					=> '#ff0000',
+			'rosybrown'				=> '#bc8f8f',
+			'royalblue'				=> '#4169e1',
+			'saddlebrown'			=> '#8b4513',
+			'salmon'				=> '#fa8072',
+			'sandybrown'			=> '#f4a460',
+			'seagreen'				=> '#2e8b57',
+			'seashell'				=> '#fff5ee',
+			'sienna'				=> '#a0522d',
+			'silver'				=> '#c0c0c0',
+			'skyblue'				=> '#87ceeb',
+			'slateblue'				=> '#6a5acd',
+			'slategray'				=> '#708090',
+			'snow'					=> '#fffafa',
+			'springgreen'			=> '#00ff7f',
+			'steelblue'				=> '#4682b4',
+			'tan'					=> '#d2b48c',
+			'teal'					=> '#008080',
+			'thistle'				=> '#d8bfd8',
+			'tomato'				=> '#ff6347',
+			'turquoise'				=> '#40e0d0',
+			'violet'				=> '#ee82ee',
+			'wheat'					=> '#f5deb3',
+			'white'					=> '#ffffff',
+			'whitesmoke'			=> '#f5f5f5',
+			'yellow'				=> '#ffff00',
+			'yellowgreen'			=> '#9acd32',
+	);
+	
+	if (isset($array_color[$color_code])) {
+		$color_code = $array_color[$color_code];
+		return ERROR_OK;
+	}
+	else {
+		return ERROR_WRONG_PRM;
+	}
 }
