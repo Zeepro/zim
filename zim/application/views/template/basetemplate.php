78e2c3cc2a07cb1ab228e47b736df177c1893f42
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-06-06 14:21:37 +0000

    use new style plugin of Paul; add some styles to adapt Paul's modification

--- zim/application/views/template/basetemplate.php
+++ zim/application/views/template/basetemplate.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 	{headers}
 	<link rel="stylesheet" href="/styles/jquery.mobile-1.3.0.min.css" />
+	<link rel="stylesheet" href="/styles/Custom-zim.min.css" />
 	<script src="/scripts/jquery-1.9.1.min.js"></script>
 	<script>
 		$(document).bind("mobileinit", function() {
@@ -13,15 +14,33 @@
 <script src="/scripts/jquery.mobile-1.3.0.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, target-densitydpi=medium-dpi, user-scalable=0" />
 
-<link rel="stylesheet" href="/assets/css/4.css">
-<link rel="stylesheet" href="/assets/css/style.css">
-<link rel="stylesheet" href="/styles/flag.css" />
+<!-- <link rel="stylesheet" href="/assets/css/4.css"> -->
+<!-- <link rel="stylesheet" href="/assets/css/style.css"> -->
+<!-- <link rel="stylesheet" href="/styles/flag.css" /> -->
 <style type="text/css">
+	div.logo {
+		width:100%; 
+		height:110px; 
+		background:url("/assets/images/logo-1.png") top center no-repeat;
+		background-size: 119px 96px;
+	}
 	div#link_logo {
 		margin: 0 auto;
 		width: 97px;
 		height: 100px;
 		cursor: pointer;
+	}
+	.ui-body-c{
+		color:#575749;
+		background:url("/assets/images/page-backgrounds/back-4.jpg") 0 -100px repeat-x  #f9f7f3;
+		background-size: 79px 245px; 
+	}
+	.page-header { 
+		background:url("/assets/images/headerBack.png") bottom repeat-x; 
+		border:0px; 
+		color:#333; 
+		height:38px; 
+		text-shadow: 0 -1px 1px #fff; 
 	}
 </style>
 <script type="text/javascript">
