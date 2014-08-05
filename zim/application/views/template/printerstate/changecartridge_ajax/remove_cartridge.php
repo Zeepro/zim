4547620fb6a02babb9e82c9bbfb5363352911f3d
Qavo7635 <Qavo7635@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-08-05 16:28:55 +0000

    Change Gif to Png

--- zim/application/views/template/printerstate/changecartridge_ajax/remove_cartridge.php
+++ zim/application/views/template/printerstate/changecartridge_ajax/remove_cartridge.php
@@ -1,5 +1,5 @@
 				<p>{action_hint}</p>
 				<p>{low_hint}</p>
-				<img src="/images/cartridge_remove.gif">
+				<img src="/images/remove-cartridge.png">
 
 <script type="text/javascript"> var_next_phase = '{next_phase}'; </script>
