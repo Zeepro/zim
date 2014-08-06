496212300c0a06394b9cfdbc70efcf35fdc1899b
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-08-06 12:55:33 +0000

    add error response code on error page

--- zim/application/controllers/error.php
+++ zim/application/controllers/error.php
@@ -1,33 +1,34 @@
 <?php
 
 if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+	exit('No direct script access allowed');
 
 class Error extends MY_Controller {
 
-    function __construct() {
-        parent::__construct();
-        $this->load->helper(array('form', 'url', 'json'));
-    }
+	function __construct() {
+		parent::__construct();
+		$this->load->helper(array('form', 'url', 'json'));
+	}
 
-    public function index()
-    {
-   		$this->load->library('parser');
-   		$this->lang->load('error', $this->config->item('language'));
-   		
-   		$data = array(
-   			'title'	=>	t('title'),
-   			'error'	=>	t('error'),
-   			'home'	=>	t('home')
-   		);
-   		
-   		$body_page = $this->parser->parse('error.php', $data, TRUE);
+	public function index()
+	{
+		$this->load->library('parser');
+		$this->lang->load('error', $this->config->item('language'));
+		
+		$data = array(
+			'title'	=>	t('title'),
+			'error'	=>	t('error'),
+			'home'	=>	t('home')
+		);
+		
+		$body_page = $this->parser->parse('error.php', $data, TRUE);
 		$template_data = array(
 				'lang'			=> $this->config->item('language_abbr'),
 				'headers'		=> '<title>' . t('ZeePro Personal Printer 21 - Home') . '</title>',
 				'contents'		=> $body_page,
 		);
+		$this->output->set_status_header(503, 'Error - Zeepro');
 		$this->parser->parse('template/basetemplate', $template_data);
-    }
+	}
 
 }
