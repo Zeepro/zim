d0280b9c2b9c52c1afd14df517f2e7e746dd6518
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-08-25 18:34:51 +0000

    re-add printerstate view subfolder

---
+++ zim/application/views/template/printerstate/changecartridge_ajax/insert_cartridge.php
@@ -0,0 +1,4 @@
+				<p>{action_hint}</p>
+				<img src="/images/insert-cartridge.png">
+
+<script type="text/javascript"> var_next_phase = '{next_phase}'; </script>
---
+++ zim/application/views/template/printerstate/changecartridge_ajax/remove_cartridge.php
@@ -0,0 +1,5 @@
+				<p>{action_hint}</p>
+				<p>{low_hint}</p>
+				<img src="/images/remove-cartridge.png">
+
+<script type="text/javascript"> var_next_phase = '{next_phase}'; </script>
