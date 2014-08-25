234e2e7f172766683788d9ded711aee97dadc365
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-08-25 18:33:15 +0000

    re-add printerstate view folder

---
+++ zim/application/views/template/printerstate/resetnetwork_finish.php
@@ -0,0 +1,15 @@
+<div data-role="page" data-url="/printerstate/resetnetwork">
+	<header data-role="header" class="page-header">
+	</header>
+	<div class="logo"><div id="link_logo"></div></div>
+	<div data-role="content">
+		<div id="container">
+			<h2 style="text-align: center;">{hint}</h2>
+			<form method="get" accept-charset="utf-8">
+				<div>
+					<input type="submit" value="{ok_button}" />
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
