bb52319e1be372b2e37c8aca12c9d3c07bdd1dba
tng <tng@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-09-05 14:16:11 +0000

    new images for cartridges handling

--- zim/application/views/template/printerstate/changecartridge_ajax/insert_cartridge.php
+++ zim/application/views/template/printerstate/changecartridge_ajax/insert_cartridge.php
@@ -1,4 +1,5 @@
 				<p>{action_hint}</p>
-				<img src="/images/insert-cartridge.png">
-
+				<img style="max-width:30%;max-height:30%" src="/images/insert-cartridge-new.jpg">
+				<img style="max-width:30%;max-height:30%" src="/images/insert-cartridge-new2.jpg">
+				<img style="max-width:30%;max-height:30%" 	src="/images/insert-cartridge-new3.jpg">
 <script type="text/javascript"> var_next_phase = '{next_phase}'; </script>
