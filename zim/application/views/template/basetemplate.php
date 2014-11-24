ae6f1a0af7b24ca78c89ac591efa1b2ff315c99a
tng <tng@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-11-24 17:49:45 +0000

    no cache

--- zim/application/views/template/basetemplate.php
+++ zim/application/views/template/basetemplate.php
@@ -4,6 +4,11 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
 		<meta http-equiv="X-UA-Compatible" content="IE=9" />
 		<meta charset="utf-8" />
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
 		{headers}
 		<link rel="stylesheet" href="/styles/jquery.mobile-1.4.0.min.css" />
  		<link rel="stylesheet" href="/styles/Custom-zim.min.css" />
