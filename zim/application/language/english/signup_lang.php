064afcc81f1987e95febb0eaab790162ca09764e
tng <tng@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2014-12-10 15:43:40 +0000

    zeepro -> Zeepro

--- zim/application/language/english/signup_lang.php
+++ zim/application/language/english/signup_lang.php
@@ -2,7 +2,7 @@
 
 $lang['already'] = "Already have a Zeepro account ?";
 $lang['back'] = 'Back';
-$lang['signup_title'] = "Create your zeepro account";
+$lang['signup_title'] = "Create your Zeepro account";
 $lang['signup_text'] = 'Your account will allow you to access your zim from anywhere, get additional free models, easier ZeeproCare Service and much more to come.';
 $lang['confcode_hint'] = 'A confirmation email will be sent to this address.';
 $lang['show_password'] = 'Show passwords';
