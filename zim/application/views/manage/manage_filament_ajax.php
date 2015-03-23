ba304d0e23d4c755c2a008a19ff546ca18f77db1
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2015-03-23 17:51:21 +0000

    add - margin top on cartridge infomation of manage page (florent)

--- zim/application/views/manage/manage_filament_ajax.php
+++ zim/application/views/manage/manage_filament_ajax.php
@@ -1,5 +1,5 @@
 
-	<img src="/images/cartridge.png" width="40%" height="40%" style="visibility:{visibility};background-color:{color};max-width:60px;max-height:60px" />
+	<img src="/images/cartridge.png" width="40%" height="40%" style="visibility:{visibility};background-color:{color};max-width:60px;max-height:60px;margin-top:5px;" />
 	{material}
 	{length}{length_text}
-	<p>{action}</p> 
+	<p>{action}</p>
