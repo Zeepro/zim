0eec002a890404b353f535ea42a8f490fa71edb9
pni <pni@f3eeed96-8428-492b-a8c0-c9c9038f9165>
2015-03-25 11:44:37 +0000

    create new branch for old zim version v1.7 (r748)

---
+++ branches/zim_v1/zim/application/controllers/set_cookie.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+class Set_cookie extends CI_Controller
+{
+	public function index()
+	{
+		$this->load->helper('printerlog');
+		if ($this->input->server('REQUEST_METHOD') == 'POST')
+		{
+			//echo '<script>document.cookie = "auth=' . $_POST['token'] . '"; console.log(document.cookie);</script>';
+			setcookie("auth", $_POST['token']);
+			//PrinterLog_logError("SetCookie: ". $_POST['token']);
+			header('Location: /');
+		}
+		else
+		{
+			PrinterLog_logError("SetCookie: method != POST");
+			echo '<script>alert("denied")</script>';
+		}
+	}
+}
---
+++ branches/zim_v1/zim/deploy.bat
@@ -0,0 +1,2 @@
+robocopy c:\inetpub\wwwroot\zim\uidev c:\inetpub\wwwroot\zim\ui *.* /e /purge /xf .* *.bat *.bak C:\inetpub\wwwroot\zim\uidev\application\config\config.php /xd .* data /m
+pause
